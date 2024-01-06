b43774e2722955b4f11a2429f8ef47a7b0635fb8
Felix Dobler <felix.dobler.fd@gmail.com>
2024-01-06 20:13:56 +0100

    combine multiple alerts into one email

--- backup_server/check_client_backup.sh
+++ backup_server/check_client_backup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-backup_dir=/backups
-alert_email=felix@doblerones.de
+read_config () {
+    jq -r $1 config.json
+}
 
-# alert threshold in seconds (e.g., 7 days = 604800 seconds)
-alert_threshold=$((86400*1))
-# TODO add threshold per client in separate json file
+backup_dir=$(read_config ".backupDir")
+alert_email=$(read_config ".email")
 
-# the file modification date shows the timestamp of the last client sync
+# the file modification date of the `HOSTNAME_lastBackup` shows the timestamp of the last client sync and can be used to detect problems with the backups
+send_alert=0
+alert_msg_header="Subject: BACKUP ALERT"
 
+# TODO iterate over all hostname in config.json instead
 for file in $backup_dir/*_lastBackup; do
     hostname=$(basename "$file" "_lastBackup")
 
@@ -21,11 +24,26 @@ for file in $backup_dir/*_lastBackup; do
     # Calculate the time difference
     time_diff=$((current_time - last_modified))
 
+    # Get threshold value for client from config
+    alert_threshold_str="$(read_config ".clients.$hostname.alertThresholdAge")"
+
+    # convert duration from json to number of seconds
+    alert_threshold=$(date -d"1970-01-01 00:00:00 UTC $alert_threshold_str" "+%s")
+
     # Compare with the threshold
     if [ "$time_diff" -gt "$alert_threshold" ]; then
-        relative_elapsed=$(date -ud "@$time_diff" +"$(( $time_diff/3600/24 )) days %H hours")
-        printf "Subject: BACKUP ALERT $hostname\n\n\"$hostname\" did not backup for $relative_elapsed" |
-        tee /dev/tty | msmtp $alert_email
+        relative_time_elapsed=$(date -ud "@$time_diff" +"$(( $time_diff/3600/24 )) days %H hours")
+
+        # change flag to enable email alert
+        send_alert=$(( $send_alert + 1 ))
+        alert_msg="$alert_msg\"$hostname\" did not backup for $relative_time_elapsed\n"
+        echo "Prepare alert: \"$hostname\" did not backup for $relative_time_elapsed"
     fi
 done
 
+if [ $send_alert -gt 0 ]; then
+    printf "$alert_msg_header\n\n$alert_msg" | msmtp $alert_email
+    echo "Sent alert"
+fi
+
+echo "Check finished"
