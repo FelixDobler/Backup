aed352eb1d5b372fda04e13a8bbb42eb6314e3a8
Felix Dobler <felix.dobler@uni-konstanz.de>
2024-02-20 02:54:17 +0100

    add email alerts to borg backups for warnings and failures

--- backup_server/borg_backup.sh
+++ backup_server/borg_backup.sh
@@ -4,7 +4,8 @@
 # Paths of the available repositories (path needs to be provided in command):
 # local /backups/borg-repo
 # remote backup:/mnt/felix-backup/borg-repo
-
+configPath=$(dirname $0)/config.json
+alert_email=$(jq -r ".email" "$configPath")
 export BORG_REPO=$1
 
 # See the section "Passphrase notes" for more infos.
@@ -59,8 +60,10 @@ if [ ${global_exit} -eq 0 ]; then
     info "Backup and Prune finished successfully"
 elif [ ${global_exit} -eq 1 ]; then
     info "Backup and/or Prune finished with warnings"
+    printf "Subject: Backup Warning\n\nBackup and/or Prune finished with warnings:\n\tRepository: $1\n\tExit Code: $global_exit" | msmtp $alert_email
 else
     info "Backup and/or Prune finished with errors"
+    printf "Subject: Backup Error\n\nBackup and/or Prune finished with errors:\n\tRepository: $1\n\tExit Code: $global_exit" | msmtp $alert_email
 fi
 
 exit ${global_exit}
